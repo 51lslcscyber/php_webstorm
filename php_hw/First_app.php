ce5df9e1e893a8d279b26fbe054fae0206c18b56
lslcyberli <2305689151@qq.com>
2022-07-06 21:34:05 +0800

    php网站试验2022 07 06

---
+++ php_hw/First_app.php
@@ -0,0 +1,18 @@
+/**
+ * Created by PhpStorm.
+ * User: 23056
+ * Date: 2022/7/6
+ * Time: 21:31
+ *///鹄思鹄想_bit森
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>PHP 第一个应用</title>
+</head>
+
+<?php
+//显示问候语消息
+echo 'Hello World!';
+?>
+
+</html>
